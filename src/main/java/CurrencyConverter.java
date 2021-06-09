9ff7003cd7dd03c19e0c3d3cdb3a5136a049a55f
Prudhvi <cprudhvi9@gmail.com>
2021-06-09 22:00:23 +0200

    commiting the wrong code and wrong test cases to check if the CI is working or not..!!

--- src/main/java/CurrencyConverter.java
+++ src/main/java/CurrencyConverter.java
@@ -4,16 +4,20 @@ public class CurrencyConverter {
     }
     public int dollar_to_rs(int dollar){
         int rs1 = dollar * 70;
-        return rs1;
+        //return rs1;
+        return 0;
     }
     public int sek_to_rs(int sek){
         int rs2 = sek * 9;
-        return rs2;
+        //return rs2;
+        return 0;
     }
     public int euro_to_rs(int euro){
         int rs3 = euro * 100;
-        return rs3;
+        //return rs3;
+        return 0;
     }
 }
 
-//this screen recording is about the environment working checking of our CI project.
+//this screen recording is about the test-driven development of my CI project...!!!
+// the above code is the main source code of my project...!!!
--- src/test/java/TestCurrencyConverter.java
+++ src/test/java/TestCurrencyConverter.java
@@ -1,8 +1,9 @@
+//lets write the code for test cases..!!
 import static org.junit.Assert.*;
-        import org.junit.BeforeClass;
-        import org.junit.Test;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
-public class TestCurrencyConverter {
+public class TestCurrencyConverter{
     private static CurrencyConverter cc;
 
     @BeforeClass
@@ -10,18 +11,22 @@ public class TestCurrencyConverter {
         cc = new CurrencyConverter();
     }
 
+    // writing test cases for the main source code to test in various ways;
     @Test
     public void test_dlr_to_rs(){
-        assertEquals(350, cc.dollar_to_rs(5));
+        assertEquals(35, cc.dollar_to_rs(5) );
     }
 
     @Test
     public void test_sek_to_rs(){
-        assertEquals(90, cc.sek_to_rs(10));
+        assertEquals(9, cc.sek_to_rs(10));
     }
 
     @Test
     public void test_euro_to_rs(){
-        assertEquals(300, cc.euro_to_rs(3));
+        assertEquals(30, cc.euro_to_rs(3));
     }
 }
+
+//finall all the test cases are ready to commit and build...
+// but first we try to build the wrong codes and test cases to check if the CI is working properly or not ....!!!
