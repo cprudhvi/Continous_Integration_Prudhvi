921e28d1dec2588a4e2535837e3283d88ffa9d34
Prudhvi <cprudhvi9@gmail.com>
2021-06-09 22:08:45 +0200

    fixed the errors in testcase 1 and rebuilding the project..!!!!

--- src/test/java/TestCurrencyConverter.java
+++ src/test/java/TestCurrencyConverter.java
@@ -14,7 +14,7 @@ public class TestCurrencyConverter{
     // writing test cases for the main source code to test in various ways;
     @Test
     public void test_dlr_to_rs(){
-        assertEquals(35, cc.dollar_to_rs(5) );
+        assertEquals(350, cc.dollar_to_rs(5) );
     }
 
     @Test
@@ -31,3 +31,5 @@ public class TestCurrencyConverter{
 //finall all the test cases are ready to commit and build...
 // but first we try to build the wrong codes and test cases to check if the CI is working properly or not ....!!!
 // now lets try to fix the errors in code and see if this works to ru nthe project...!!!
+// it is shown that the problem is with test cases not with the code..!!!
+// now lets try to fix the test case 1 and rebuild the project..!!!!!!
