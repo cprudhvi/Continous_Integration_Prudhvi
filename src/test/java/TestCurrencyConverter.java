2cd94f936ba9c5171684d5b280bda51fdba224ab
Prudhvi <cprudhvi9@gmail.com>
2021-06-09 21:22:08 +0200

    adding test cases

--- src/test/java/TestCurrencyConverter.java
+++ src/test/java/TestCurrencyConverter.java
@@ -1,2 +1,27 @@
+import static org.junit.Assert.*;
+        import org.junit.BeforeClass;
+        import org.junit.Test;
+
 public class TestCurrencyConverter {
+    private static CurrencyConverter cc;
+
+    @BeforeClass
+    public static void setup(){
+        cc = new CurrencyConverter();
+    }
+
+    @Test
+    public void test_dlr_to_rs(){
+        assertEquals(350, cc.dollar_to_rs(5));
+    }
+
+    @Test
+    public void test_sek_to_rs(){
+        assertEquals(90, cc.sek_to_rs(10));
+    }
+
+    @Test
+    public void test_euro_to_rs(){
+        assertEquals(300, cc.euro_to_rs(3));
+    }
 }
