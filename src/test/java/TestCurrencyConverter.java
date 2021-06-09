d5deace7d2651f7af83092f89b133a404b844044
Prudhvi <cprudhvi9@gmail.com>
2021-06-09 22:12:58 +0200

    fixed the errors in testcase 2 and rebuilding the project..!!!!

--- src/test/java/TestCurrencyConverter.java
+++ src/test/java/TestCurrencyConverter.java
@@ -1,4 +1,4 @@
-//lets write the code for test cases..!!
+
 import static org.junit.Assert.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -14,12 +14,12 @@ public class TestCurrencyConverter{
     // writing test cases for the main source code to test in various ways;
     @Test
     public void test_dlr_to_rs(){
-        assertEquals(350, cc.dollar_to_rs(5) );
+        assertEquals(350, cc.dollar_to_rs(5));
     }
 
     @Test
     public void test_sek_to_rs(){
-        assertEquals(9, cc.sek_to_rs(10));
+        assertEquals(90, cc.sek_to_rs(10));
     }
 
     @Test
@@ -33,3 +33,4 @@ public class TestCurrencyConverter{
 // now lets try to fix the errors in code and see if this works to ru nthe project...!!!
 // it is shown that the problem is with test cases not with the code..!!!
 // now lets try to fix the test case 1 and rebuild the project..!!!!!!
+//now lets do the same thing with testcase 2 fix it and rebuild the proejct...!!!!
