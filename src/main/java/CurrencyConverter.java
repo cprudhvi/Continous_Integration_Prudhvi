4909a4314f1969066856bea15231130e5ef53af6
Prudhvi <cprudhvi9@gmail.com>
2021-06-30 23:20:42 +0200

    test case 1

--- src/main/java/CurrencyConverter.java
+++ src/main/java/CurrencyConverter.java
@@ -2,6 +2,10 @@ public class CurrencyConverter {
     public CurrencyConverter(){
 
     }
+    public int dollar_to_rs(int dollar){
+        int rs1 = dollar *70;
+        return rs1;
+    }
 
 }
 
