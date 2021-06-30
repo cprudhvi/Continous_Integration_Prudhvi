a702fc61aff1376c1b44b51f74d9adcfe10870fd
Prudhvi <cprudhvi9@gmail.com>
2021-06-30 23:26:34 +0200

    test case 1 with out code.

--- src/main/java/CurrencyConverter.java
+++ src/main/java/CurrencyConverter.java
@@ -2,10 +2,21 @@ public class CurrencyConverter {
     public CurrencyConverter(){
 
     }
-    public int dollar_to_rs(int dollar){
-        int rs1 = dollar *70;
-        return rs1;
-    }
 
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// no code is written for the test case 1.
--- src/test/java/TestCurrencyConverter.java
+++ src/test/java/TestCurrencyConverter.java
@@ -11,7 +11,7 @@ public class TestCurrencyConverter{
         cc = new CurrencyConverter();
     }
 
-    //
+    // test case 1
     @Test
     public void test_dlr_to_rs(){
         assertEquals(350, cc.dollar_to_rs(5));
@@ -19,3 +19,6 @@ public class TestCurrencyConverter{
 
 }
 
+// this recording is about Test driven development in ST_final Project.
+// first i am going to write the test case and then commit, push build in jenkins.
+//
