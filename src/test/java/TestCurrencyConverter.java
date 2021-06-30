5a5d8cdc979817a5b1771593adbb077f4218779c
Prudhvi <cprudhvi9@gmail.com>
2021-06-30 23:06:38 +0200

    TDD

--- src/test/java/TestCurrencyConverter.java
+++ src/test/java/TestCurrencyConverter.java
@@ -11,7 +11,7 @@ public class TestCurrencyConverter{
         cc = new CurrencyConverter();
     }
 
-    //
+    // Test
     @Test
     public void test_dlr_to_rs(){
         assertEquals(350, cc.dollar_to_rs(5));
