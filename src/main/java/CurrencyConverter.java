3ff7a2b08dbd58acda0c7f5441eda77dac4843d9
Prudhvi <cprudhvi9@gmail.com>
2021-06-30 22:59:27 +0200

    TDD

--- src/main/java/CurrencyConverter.java
+++ src/main/java/CurrencyConverter.java
@@ -2,21 +2,6 @@ public class CurrencyConverter {
     public CurrencyConverter(){
 
     }
-    public int dollar_to_rs(int dollar){
-        int rs1 = dollar * 70;
-        return rs1;
-    }
-    public int sek_to_rs(int sek){
-        int rs2 = sek * 9;
-        return rs2;
-    }
-    public int euro_to_rs(int euro){
-        int rs3 = euro * 100;
-        return rs3;
 
-    }
 }
 
-//this screen recording is about the test-driven development of my CI project...!!!
-// the above code is the main source code of my project...!!!
-// i have fixed the error in the code and lets try to build the project now..!!!
--- src/test/java/TestCurrencyConverter.java
+++ src/test/java/TestCurrencyConverter.java
@@ -11,27 +11,11 @@ public class TestCurrencyConverter{
         cc = new CurrencyConverter();
     }
 
-    // writing test cases for the main source code to test in various ways;
+    //
     @Test
     public void test_dlr_to_rs(){
         assertEquals(350, cc.dollar_to_rs(5));
     }
 
-    @Test
-    public void test_sek_to_rs(){
-        assertEquals(90, cc.sek_to_rs(10));
-    }
-
-    @Test
-    public void test_euro_to_rs(){
-        assertEquals(300, cc.euro_to_rs(3));
-    }
 }
 
-//finall all the test cases are ready to commit and build...
-// but first we try to build the wrong codes and test cases to check if the CI is working properly or not ....!!!
-// now lets try to fix the errors in code and see if this works to ru nthe project...!!!
-// it is shown that the problem is with test cases not with the code..!!!
-// now lets try to fix the test case 1 and rebuild the project..!!!!!!
-//now lets do the same thing with testcase 2 fix it and rebuild the proejct...!!!!
-// now we are left with only one wrong test case lets fix the testcase 3 and rebuild the project ....!!!!
